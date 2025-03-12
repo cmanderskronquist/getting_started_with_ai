e47c8bf95bba5d0ded0e75500c9e476b0321c133
SV <styrbjorn.vejdal@skill-it.se>
2025-03-12 19:51:17 +0100

    More testing TLD and labels

--- colab/main.py
+++ colab/main.py
@@ -2,7 +2,7 @@
 
 from helloworld import hello
 from gtts import gTTS
-from IPython.display import Audio
+from IPython.display import Audio, display, HTML
 
 tts = gTTS('The quick brown fox jumps over the lazy dog.', tld='co.uk')
 tts.save('uk.mp3')
@@ -10,7 +10,15 @@ tts = gTTS('The quick brown fox jumps over the lazy dog.', tld='com')
 tts.save('us.mp3')
 tts = gTTS('The quick brown fox jumps over the lazy dog.', tld='com.au')
 tts.save('au.mp3')
+tts = gTTS('The quick brown fox jumps over the lazy dog.', tld='co.in')
+tts.save('in.mp3')
 
+display(HTML('<p>UK:</p>'))
 display(Audio('uk.mp3', autoplay=True))
+
+display(HTML('<p>US:</p>'))
 display(Audio('us.mp3', autoplay=False))
+display(HTML('<p>AU:</p>'))
 display(Audio('au.mp3', autoplay=False))
+display(HTML('<p>IN:</p>'))
+display(Audio('in.mp3', autoplay=False))
