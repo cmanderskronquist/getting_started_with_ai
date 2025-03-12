48e29d3cac07cefd8147f485021997c9ce5d883a
SV <styrbjorn.vejdal@skill-it.se>
2025-03-12 19:33:42 +0100

    Testing Google TTS

--- colab/main.py
+++ colab/main.py
@@ -1,5 +1,11 @@
 #!/bin/python3
 
 from helloworld import hello
+from gtts import gTTS
+from IPython.display import Audio
+tts = gTTS('Hello, this is a test of text-to-speech.')
+tts.save('test.mp3')
 
-hello()
+#hello()
+
+display(Audio('test.mp3', autoplay=True))
