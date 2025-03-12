eb5d5025746c7dfb5877c42bc97507caf10e8f81
SV <styrbjorn.vejdal@skill-it.se>
2025-03-12 19:43:01 +0100

    Changing locales

--- colab/main.py
+++ colab/main.py
@@ -3,9 +3,14 @@
 from helloworld import hello
 from gtts import gTTS
 from IPython.display import Audio
-tts = gTTS('Hello, this is a test of text-to-speech.')
-tts.save('test.mp3')
 
-#hello()
+tts = gTTS('The quick brown fox jumps over the lazy dog.', lang='en-uk')
+tts.save('uk.mp3')
+tts = gTTS('The quick brown fox jumps over the lazy dog.', lang='en-us')
+tts.save('us.mp3')
+tts = gTTS('The quick brown fox jumps over the lazy dog.', lang='en-au')
+tts.save('au.mp3')
 
-display(Audio('test.mp3', autoplay=True))
+display(Audio('uk.mp3', autoplay=True))
+display(Audio('us.mp3', autoplay=False))
+display(Audio('au.mp3', autoplay=False))
