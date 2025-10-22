5dd8d2da12618af50501e1291376451c8a4b2dad
SV <styrbjorn.vejdal@skill-it.se>
2025-10-22 17:27:20 +0200

    playAudio works

---
+++ TTS/playAudio.py
@@ -0,0 +1,18 @@
+#
+import sounddevice as sd   
+
+def playMyAudio(file):
+    """
+    Play audio from a given file using sounddevice.
+    """
+    from scipy.io import wavfile
+
+    # Read the WAV file
+    sample_rate, data = wavfile.read(file)
+
+    # Play the audio
+    sd.play(data, samplerate=sample_rate)
+    sd.wait()  # Wait until the file is done playing    
+
+
+playMyAudio("/Users/sv/skill-AI/getting_started_with_ai/TTS/audio.wav")
